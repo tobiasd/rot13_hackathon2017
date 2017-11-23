65cd38cee6d3e8765ddf8438112267faf843c42e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-11-23 07:19:59 +0100

    initial  addon PHP file

---
+++ rot13.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Name: ROT13 Substitution Code
+ * Description: Apply rot13 to send postings. DO NOT USE FOR SECURITY/PRIACY REASONS!
+ * Version: 0.1
+ * Author: Tobias Diekershoff <https://social.diekershoff.de/profile/tobias>
+ *
+ * License: MIT License (see LICENSE file for details)
+ **/
+
+/**
+ * @brief register hooks for the addon
+ **/
+function rot13_install() {
+}
+
+/**
+ * @brief unregister hooks for the addon
+ **/
+function rot13_unistall() {
+}
