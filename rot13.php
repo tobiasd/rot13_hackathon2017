0832d673482a17e851afaa6c3fdcba4224cf7039
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-11-23 07:36:54 +0100

    added raw function to the addon

--- rot13.php
+++ rot13.php
@@ -12,10 +12,25 @@
  * @brief register hooks for the addon
  **/
 function rot13_install() {
+	register_hook('post_local', 'addon/rot13/rot13.php', 'rot13_post_local');
+	register_hook('jot_networks', 'addon/rot13/rot13.php', 'rot13_jot_networks');
 }
 
 /**
  * @brief unregister hooks for the addon
  **/
 function rot13_unistall() {
+	unregister_hook('post_local', 'addon/rot13/rot13.php', 'rot13_post_local');
+	unregister_hook('jot_networks', 'addon/rot13/rot13.php', 'rot13_jot_networks');
+}
+
+/**
+ * @brief add a checkbox to use the ROT13 to the ACL dialog
+ **/
+function rot13_jot_networks() {
+}
+/**
+ * @brief apply ROT13 to a locally made posting
+ **/
+function rot13_post_local() {
 }
